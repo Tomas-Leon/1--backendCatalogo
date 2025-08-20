2e6661b170feb4f2803f99ae8bf9bae1b91a67a6
tomas <toomiileon@gmail.com>
2025-08-20 05:35:00 -0300

    dividimos la logica de las rutas y el controlador

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import Server from "./server/config.js";
+import routerIndex from "./src/routes/index.routes.js";
 
 const server = new Server();
 
 //agregar las rutas
-
+server.app.use('/api', routerIndex)
 server.listen()
